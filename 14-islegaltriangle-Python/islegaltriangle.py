62eed942f9139aa58cbf1b4d637332b448ccbb84
Vamsi Krishna Ramisetti <vk19999052563765@msitprogram.net>
2021-07-14 15:25:34 +0530

    islegaltriangle solution

--- 14-islegaltriangle-Python/islegaltriangle.py
+++ 14-islegaltriangle-Python/islegaltriangle.py
@@ -6,5 +6,9 @@
 # how can you determine the longest side, and how might that help?
 
 def islegaltriangle(s1, s2, s3):
-	# your code goes here
-	pass
+	if(s1+s2)>s3:
+		if(s3+s2)>s1:
+			if(s1+s3)>s2:
+				return True
+	else:
+		return False
