e5ad7f40ce5a6cccd2c6f1c4df3542d0ccfdcdbc
Vamsi Krishna Ramisetti <vk19999052563765@msitprogram.net>
2021-08-03 18:40:16 +0530

    sayhi solution

--- SayHi.py
+++ SayHi.py
@@ -4,17 +4,16 @@
 # sayHi(d1) # Hi, my name is Dot and I am 4 years old!
 # sayHi(d2) # Hi, my name is Elf and I am 3 years old!
 
-# class SayHi(Object):
-
 class Dog(object):
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     # Now we are using self, as convention requires:
-    def sayHi(self):
+    def sayHI(self):
         print(f'Hi, my name is {self.name} and I am {self.age} years old!')
 
 d1 = Dog('Snoopy', 4)
-
-d1.sayHi() 
+d2 = Dog('Hash',5)
+d1.sayHI() 
+d2.sayHI()
